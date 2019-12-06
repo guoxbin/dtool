ac38ba210a748f25a9101f0236ef8fb9320dd963
guoxbin <guoxbin@gmail.com>
2019-12-07 00:54:28 +0800

    Timestamp convertion takes timezone parameter

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ dtool is a collection of development tools:
 | ------------- | :------------- | ------------ |
 | h2s    | Convert hex to string       | $ dtool h2s 0x61626364 <br> abcd  |
 | s2h    | Convert string to hex       | $ dtool h2s abcd <br> 0x61626364 |
-| ts2d   | Convert timestamp to date   | $ dtool ts2d 10000 <br> 1970-01-01 10:46:40 |
-| d2ts   | Convert date to timestamp   | $ dtool d2ts 1970-01-01 10:46:40 <br> 10000 |
+| ts2d   | Convert timestamp to date   | $ dtool ts2d 10000 <br> 1970-01-01 10:46:40 <br> $ dtool ts2d -z0 10000 <br> 1970-01-01 02:46:40 |
+| d2ts   | Convert date to timestamp   | $ dtool d2ts '1970-01-01 10:46:40' <br> 10000 <br> $ dtool d2ts -z0 '1970-01-01 02:46:40' <br> 10000 |
 | d2ts   | Number system               | $ dtool ns 256 <br> 256 <br> 0b100000000 <br> 0o400 <br> 0x100 <br> |
 | h2b58  | Convert hex to base58       | $ dtool h2b58 0x0075774f5d9963c021009a58d7d2d8e83771dd6c7a <br> 12dvBhvPEPniQmBmgvj4qpJEodT7P |
 | h2b58c | Convert hex to base58 check | $ dtool h2b58c 0x0075774f5d9963c021009a58d7d2d8e83771dd6c7a <br> 1Bi6zFVNtntP5MtDraNrAD7e469ifsQMwF |
--- src/modules/time.rs
+++ src/modules/time.rs
@@ -2,20 +2,32 @@ use clap::{SubCommand, Arg, ArgMatches};
 use crate::modules::Command;
 use std::io;
 use std::io::BufRead;
-use chrono::{NaiveDateTime, Local};
+use chrono::{NaiveDateTime, Local, FixedOffset};
 use chrono::offset::TimeZone;
 
 pub fn commands<'a, 'b>() -> Vec<Command<'a, 'b>> {
 	vec![
 		Command {
-			app: SubCommand::with_name("ts2d").about("Convert timestamp to date").arg(
+			app: SubCommand::with_name("ts2d").about("Convert timestamp to date")
+				.arg(
+					Arg::with_name("z")
+						.short("z").help("Time zone: 8(CN), 0(UK), etc")
+						.takes_value(true)
+						.required(false))
+				.arg(
 				Arg::with_name("INPUT")
 					.required(false)
 					.index(1)),
 			f: ts2d,
 		},
 		Command {
-			app: SubCommand::with_name("d2ts").about("Convert date to timestamp").arg(
+			app: SubCommand::with_name("d2ts").about("Convert date to timestamp")
+				.arg(
+					Arg::with_name("z")
+						.short("z").help("Time zone: 8(CN), 0(UK), etc")
+						.takes_value(true)
+						.required(false))
+				.arg(
 				Arg::with_name("INPUT")
 					.required(false)
 					.index(1)),
@@ -32,8 +44,21 @@ fn ts2d(matches: &ArgMatches) -> Result<Vec<String>, String> {
 	};
 
 	let timestamp : i64 = input.parse().map_err(|_| "Invalid input")?;
-	let time = Local.timestamp(timestamp, 0);
-	let result = time.format("%Y-%m-%d %H:%M:%S").to_string();
+
+	let timezone = matches.value_of("z");
+
+	let result = match timezone {
+		Some(timezone) => {
+			let timezone : i32 = timezone.parse().map_err(|_|"Invalid input")?;
+			if timezone > 12 || timezone < -12 {
+				return Err("Invalid timezone".to_string())
+			}
+			FixedOffset::east(timezone * 3600).timestamp(timestamp, 0).format("%Y-%m-%d %H:%M:%S").to_string()
+		},
+		None => {
+			Local.timestamp(timestamp, 0).format("%Y-%m-%d %H:%M:%S").to_string()
+		}
+	};
 
 	Ok(vec![result])
 }
@@ -45,9 +70,25 @@ fn d2ts(matches: &ArgMatches) -> Result<Vec<String>, String> {
 		None => io::stdin().lock().lines().map(|l|l.unwrap()).collect::<Vec<String>>().join(""),
 	};
 
+	let timezone = matches.value_of("z");
+
 	let time = NaiveDateTime::parse_from_str(&input, "%Y-%m-%d %H:%M:%S").map_err(|_| "Invalid input")?;
-	let time = Local.from_local_datetime(&time).unwrap();
-	let result = time.timestamp();
+
+	let result = match timezone {
+		Some(timezone) => {
+			let timezone : i32 = timezone.parse().map_err(|_|"Invalid input")?;
+			if timezone > 12 || timezone < -12 {
+				return Err("Invalid timezone".to_string())
+			}
+			let time = FixedOffset::east(timezone * 3600).from_local_datetime(&time).unwrap();
+			time.timestamp()
+		},
+		None => {
+			let time = Local.from_local_datetime(&time).unwrap();
+			time.timestamp()
+		}
+	};
+
 	let result = format!("{}", result);
 
 	Ok(vec![result])
@@ -62,10 +103,10 @@ mod tests {
 	fn test_ts2d() {
 		let app =  &commands()[0].app;
 
-		let matches = app.clone().get_matches_from(vec!["ts2d", "0"]);
+		let matches = app.clone().get_matches_from(vec!["ts2d", "-z8", "0"]);
 		assert_eq!(ts2d(&matches) , Ok(vec!["1970-01-01 08:00:00".to_string()]));
 
-		let matches = app.clone().get_matches_from(vec!["ts2d", "10000"]);
+		let matches = app.clone().get_matches_from(vec!["ts2d", "-z8", "10000"]);
 		assert_eq!(ts2d(&matches) , Ok(vec!["1970-01-01 10:46:40".to_string()]));
 
 	}
@@ -74,10 +115,10 @@ mod tests {
 	fn test_d2ts() {
 		let app =  &commands()[1].app;
 
-		let matches = app.clone().get_matches_from(vec!["d2ts", "1970-01-01 08:00:00"]);
+		let matches = app.clone().get_matches_from(vec!["d2ts", "-z8", "1970-01-01 08:00:00"]);
 		assert_eq!(d2ts(&matches) , Ok(vec!["0".to_string()]));
 
-		let matches = app.clone().get_matches_from(vec!["d2ts", "1970-01-01 10:46:40"]);
+		let matches = app.clone().get_matches_from(vec!["d2ts", "-z8", "1970-01-01 10:46:40"]);
 		assert_eq!(d2ts(&matches) , Ok(vec!["10000".to_string()]));
 
 	}
